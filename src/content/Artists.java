bb7bfa19aff8ba5ff8149a47065cd3c8ea9da4bd
Vadim <baklamakla@gmail.com>
2013-10-25 13:58:48 +0300

    BlockTitles class added

---
+++ src/content/Artists.java
@@ -0,0 +1,38 @@
+package content;
+
+import org.openqa.selenium.By;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import basic.BasicTestCase;
+
+public class Artists extends BasicTestCase {
+	@Test(priority=1, description="Проверяет отображение артистов на <<Музыка>>")
+	public void MusicArtists () throws Exception {
+	  driver.get(musicUrl);
+	  checkArtists(15);
+}
+	@Test(priority=2, description="Проверяет отображение артистов на <<Новинки>>")
+	public void NovinkiArtists () throws Exception {
+	  driver.get(novinkiUrl);
+	  checkArtists(7);
+}
+	@Test(priority=3, description="Проверяет отображение артистов на <<Казахстанская>>")
+	public void KazahArtists () throws Exception {
+	  driver.get(kazahUrl);
+	  checkArtists(15);
+}
+	//check artists is present
+	public void checkArtists (int expected_numb_artists) throws Exception {
+	    int real_numb_artists = driver.findElements(By.cssSelector(".ordered-list>li")).size();
+	    for (int i=1; i<=real_numb_artists; i++) { 
+	    driver.findElement(By.xpath("//*[@class='ordered-list']//li["+i+"]/h4/a")).isDisplayed();
+	     }
+	    if (expected_numb_artists != real_numb_artists) {
+	    	 System.out.println("На странице "+driver.getCurrentUrl()+" неотображаются артисты");
+		     Reporter.log("На странице "+driver.getCurrentUrl()+" неотображаются артисты");
+		     throw new NullPointerException ();
+	    }
+	} 
+
+}
---
+++ src/content/BlokTitles.java
@@ -0,0 +1,86 @@
+package content;
+
+
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import basic.BasicTestCase;
+
+public class BlokTitles extends BasicTestCase {
+	 //Check title on a block
+		@Test(priority=1, description="Проверяет отображение имени блока на <<Музыка>>")
+		public void MusicTitles () throws Exception {
+			  driver.get(musicUrl);
+			  	try{
+			  		driver.findElement(By.xpath("//h3[contains(text(),'Топ-исполнители')]")).isDisplayed();
+		  		}
+				  catch (NoSuchElementException e) {
+					    System.out.println("На странице "+driver.getCurrentUrl()+" неотображается имя блока  <<Топ-исполнители>>");
+				    	Reporter.log("На странице "+driver.getCurrentUrl()+" неотображается имя блока  <<Топ-исполнители>>");
+				  		throw new NoSuchElementException ("");
+				   		}
+			  	try{
+			  		driver.findElement(By.xpath("//h3[contains(text(),'Топ-10 треков за неделю')]")).isDisplayed();
+				}
+				  catch (NoSuchElementException e) {
+					    System.out.println("На странице "+driver.getCurrentUrl()+"неотображается имя блока <<Топ-10 треков за неделю>>");
+				    	Reporter.log("На странице "+driver.getCurrentUrl()+"неотображается имя блока <<Топ-10 треков за неделю>>");
+				  		throw new NoSuchElementException ("");
+				   		}
+			  	try{
+			  		driver.findElement(By.xpath("//h3[contains(text(),'Популярные жанры')]")).isDisplayed();
+			  	}
+			  		 catch (NoSuchElementException e) {
+						    System.out.println("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Популярные жанры>>");
+					    	Reporter.log("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Популярные жанры>>");
+					  		throw new NoSuchElementException ("");
+					   		}
+	}
+/*--------------------------------------------------------------------------------------------------------------------------------------------*/
+		
+		 @Test(priority=2, description="Проверяет отображение имен блоков на  <<Новинки>>")
+		 public void NovinkiTitles () throws Exception {
+			 driver.get(novinkiUrl);
+			 try {
+			 driver.findElement(By.xpath("//h3[contains(text(),'Новые популярные исполнители')]")).isDisplayed();
+			 } catch (NoSuchElementException e) {
+				    System.out.println("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Новые популярные исполнители>>");
+			    	Reporter.log("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Новые популярные исполнители>>");
+			  		throw new NoSuchElementException (""); 
+			 }
+		 }
+/*----------------------------------------------------------------------------------------------------------------------------------------------*/
+		 @Test(priority=3, description="Проверяет отображение имен блоков на  <<Казахстанская>>")
+		 public void KazahTitles () throws Exception {
+			 driver.get(kazahUrl);
+			 try{
+				 driver.findElement(By.xpath("//h3[contains(text(),'Топ-Исполнители Казахстана')]")).isDisplayed();
+			 }
+				 catch (NoSuchElementException e) {
+					    System.out.println("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Топ-Исполнители Казахстана>>");
+				    	Reporter.log("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Топ-Исполнители Казахстана>>");
+				  		throw new NoSuchElementException (""); 
+				 }
+			 try{
+				 driver.findElement(By.xpath("//h3[contains(text(),'Популярные жанры')]")).isDisplayed();
+		 			}
+				 catch (NoSuchElementException e) {
+					    System.out.println("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Популярные жанры>>");
+				    	Reporter.log("На странице "+driver.getCurrentUrl()+" неотображается имя блока <<Популярные жанры>>");
+				  		throw new NoSuchElementException (""); 
+				 }
+			 try{
+				 driver.findElement(By.xpath("//h3[contains(text(),'Топ-10 казахских песен за неделю')]")).isDisplayed();
+			   	}
+			 catch (NoSuchElementException e) {
+				    System.out.println("На странице "+driver.getCurrentUrl()+" неторображается имя блока <<Топ-10 казахских песен за неделю>>");
+			    	Reporter.log("На странице "+driver.getCurrentUrl()+" неторображается имя блока <<Топ-10 казахских песен за неделю>>");
+			  		throw new NoSuchElementException (""); 
+			 }
+		   
+		
+	}
+}
