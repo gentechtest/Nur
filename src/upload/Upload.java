f007c35908d3bd5f5b3a56500f39f66fa9e76c37
Vadim <baklamakla@gmail.com>
2013-10-30 12:33:42 +0200

    Fix Upload class

--- src/upload/Upload.java
+++ src/upload/Upload.java
@@ -29,7 +29,7 @@ public class Upload extends BasicTestCase {
 	assertPage(musicUrl);
 	obj.Upload.click(); //go to upload link on head
 	setClipboard("C:\\test.mp3");
-	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'SWFUpload_0')]"))).click();//click on upload button
+	driver.findElement(By.xpath("//*[contains(@id,'SWFUpload_0')]")).click();//click on upload button
 	keySendToWinForm();
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='file_complete_status']/a"))).click(); // go to play list after download
 	driver.navigate().refresh();
