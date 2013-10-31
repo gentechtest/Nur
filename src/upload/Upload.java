6073bdcf6da6187b217a1dbe80682782c2d7c978
Vadim <baklamakla@gmail.com>
2013-10-31 16:06:16 +0200

    Upload fixed

--- src/upload/Upload.java
+++ src/upload/Upload.java
@@ -24,7 +24,7 @@ public class Upload extends BasicTestCase {
 	assertPage(musicUrl);
 	obj.Upload.click(); //go to upload link on head
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'SWFUpload_0')]"))).click();//click on upload button
-	Runtime.getRuntime().exec("resource/upload_scrpt.exe"); // use script 
+	Runtime.getRuntime().exec("C:/upload_scrpt.exe"); // use script 
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='file_complete_status']/a"))).click(); // go to play list after download
 	driver.navigate().refresh();
 	assertEquals("Неизвестный исполнитель — test" ,driver.findElement(By.xpath("//*[@class='left']//*[@class='advanced_playlist']/li[1]")).getAttribute("audio_name")); //check track
