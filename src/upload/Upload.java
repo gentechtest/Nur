b2e4a43cb132af533a80e40e6abd4c7cbe7a5a03
Vadim <baklamakla@gmail.com>
2013-10-31 14:56:39 +0200

    Upload class added

--- src/upload/Upload.java
+++ src/upload/Upload.java
@@ -1,7 +1,6 @@
 package upload;
 
 import static org.testng.AssertJUnit.assertEquals;
-
 import java.awt.AWTException;
 import java.awt.Robot;
 import java.awt.Toolkit;
@@ -18,38 +17,21 @@ import pageFactory.ContentPage;
 import basic.BasicTestCase;
 
 public class Upload extends BasicTestCase {
-	//Функция для копирования в обуфер обмена Windows
-	public static void setClipboard(String str) {
-	    StringSelection ss = new StringSelection(str);
-	    Toolkit.getDefaultToolkit().getSystemClipboard().setContents(ss, null);
-	}
 	private ContentPage obj = PageFactory.initElements(getWebDriver(), ContentPage.class);
-	
-	
-WebDriverWait wait = new WebDriverWait(driver, 30);
+	WebDriverWait wait = new WebDriverWait(driver, 30);
 
-//Функция для отправки строки из буфера обмена в Win форму (аля на хуй скрипт)
-public void keySendToWinForm () throws AWTException {
-	Robot robot = new Robot();
-         robot.delay(3000);
-         robot.keyPress(KeyEvent.VK_CONTROL);
-         robot.keyPress(KeyEvent.VK_V);
-         robot.keyRelease(KeyEvent.VK_V);
-         robot.keyRelease(KeyEvent.VK_CONTROL);
-         robot.keyPress(KeyEvent.VK_ENTER);
-}
+
 //Upload track                     
-@Test(priority = 1, groups={"Upload"}, description="Проверяет загрузку треков на сайт")
+@Test(priority = 1, groups={"Upload"}, description="Проверяет загрузку треков на сайт", dependsOnGroups={"Login"})
 	public void UploadTrack () throws Exception {
 	driver.get(musicUrl);
 	assertPage(musicUrl);
 	obj.Upload.click(); //go to upload link on head
-	setClipboard("C:\\test.mp3");
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[contains(@id,'SWFUpload_0')]"))).click();//click on upload button
-	keySendToWinForm();
+	Runtime.getRuntime().exec("resource/upload_scrpt.exe"); // use script 
 	wait.until(ExpectedConditions.elementToBeClickable(By.xpath(".//*[@id='file_complete_status']/a"))).click(); // go to play list after download
 	driver.navigate().refresh();
 	assertEquals("Неизвестный исполнитель — test" ,driver.findElement(By.xpath("//*[@class='left']//*[@class='advanced_playlist']/li[1]")).getAttribute("audio_name")); //check track
-	obj.DelTrack.click(); //delete track from play list
+	obj.DelTrack.click(); //delete track from play list*/
 	}
 }
