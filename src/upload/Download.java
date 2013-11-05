4e248ff192cf121fa8049892403a6ab0f87c4c3e
Vadim <baklamakla@gmail.com>
2013-11-05 13:20:19 +0200

    Download added

--- src/upload/Download.java
+++ src/upload/Download.java
@@ -2,71 +2,123 @@ package upload;
 
 import java.awt.Toolkit;
 import java.io.File;
-import java.nio.file.FileSystems;
-import java.nio.file.Path;
-import java.util.Set;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.TimeoutException;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Reporter;
 import org.testng.annotations.Test;
 
-import pageFactory.ContentPage;
 import basic.BasicTestCase;
 
 public class Download extends BasicTestCase {
-	private ContentPage obj = PageFactory.initElements(getWebDriver(), ContentPage.class);
 	
 	@Test(description ="Проверяет скачку треков ")
 	public void download () throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, 180);
 		driver.get(musicUrl);
-		System.out.println(Toolkit.getDefaultToolkit().getScreenResolution());
-		/*String TrackTitle = obj.FirstTrack.getAttribute("audio_name");//gets name of track that will be download
-		String a= TrackTitle.replace(" ", "+").replace("—", "-");//reconstruct track name
-		String c= a+"(music.nur.kz)";//adds string
-		File file = new File("C:\\Users\\Voice\\Downloads\\"+c+".mp3"); // file path
-	    //Switch to new window
-		final Set<String> oldWindowsSet = driver.getWindowHandles();
-        
-		driver.findElement(By.xpath("//*[@class='addthis control download download-track-counter']")).click();//download track	
-		WebDriver driver = BasicTestCase.getWebDriver();
-		String newWindow = (new WebDriverWait(driver, 10))
-	            .until(new ExpectedCondition<String>() {
-	                public String apply(WebDriver driver) {
-	                    Set<String> newWindowsSet = driver.getWindowHandles();
-	                    newWindowsSet.removeAll(oldWindowsSet);
-	                    return newWindowsSet.size() > 0 ? 
-	                                 newWindowsSet.iterator().next() : null;
-	                  }
-	                }
-	            );
-	     driver.switchTo().window(newWindow);
+		driver.findElement(By.xpath("//*[@class='addthis control download download-track-counter']")).click();//download track
+		
+		//Check PC
+		System.out.println(Toolkit.getDefaultToolkit().getScreenResolution()); 
+		
+		//Switch to new window opened
+		for(String winHandle : driver.getWindowHandles()){
+		    driver.switchTo().window(winHandle);
+		}
 	      try {  
 	     driver.findElement(By.xpath("//*[@class='one-track-block']/a[1]")).click(); // click on download link
 	      }
 	      catch (NoSuchElementException e) {
-	    	  System.out.println("На странице "+driver.getCurrentUrl()+ " неотображается ссылка <<Скачать песню>>");
+	    	  Reporter.log("На странице "+driver.getCurrentUrl()+ " неотображается ссылка <<Скачать песню>>",true);
 	      }
-		//wait, while track doesn't download, but only 3 minutes 
-		int i =0;//set timer to zero
-		while (!file.canRead()) { //while file can't read file timer +1 second
-		i++;
-		Thread.sleep(1000);
-		if (i>=120) { // max wait 180 seconds
-			System.out.println("Время ожидания загрузки истекло "); // if file downloading more than 180 seconds print error!!
-			Reporter.log("Время ожидания загрузки истекло");
-		break; // out of "while"
-			}
-		}
-	    if  (!file.exists()) {
-	    	System.out.println("Трек не найден");
-	    	Reporter.log("Трек не нейден");
-	    	throw new NullPointerException(); // check if file not existing print error
-	    }
-	    	else file.delete(); // if exist delete file    */
+	      Thread.sleep(2000);
+	      driver.get("chrome://downloads/");
+	      String stats= driver.findElement(By.xpath("//*[@class='status']")).getText();
+	      System.out.println(stats);
+	      try {
+	      wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class='controls']/a[1]")));
+	      }
+	      catch (TimeoutException e) {
+	    	  Reporter.log("Трек не скачался",true);
+	    	  throw new NullPointerException ();
+	      }
+	      String fileName = driver.findElement(By.xpath("//*[@class='title-area']/a")).getText();
+	      File file = new File("C:\\Users\\Voice\\Downloads\\"+fileName); // file path
+	      System.out.println(file);
+	      if  (!file.exists()) {
+		    	System.out.println("Трек не найден");
+		    	Reporter.log("Трек не нейден");
+		    	throw new NullPointerException(); // check if file not existing print error
+		    }    // if exist delete file   
+		    	else file.delete();
+	      		System.out.println("Трек успешно скачан, и был удален");
+	      
+	   
+	 
+	
 	}
+	public void  DownloadSpeed () {  
+		
+		 String starts= driver.findElement(By.xpath("//*[@class='status']")).getText();
+	      if(starts.contains("–")) {
+	    	  	
+	    	  int end = starts.indexOf("–");
+		      int start = 0;
+		      char buf[] = new char[end - start];
+		      starts.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	      else if (starts.contains("-")) {
+	    	  
+	    	  int end = starts.indexOf("–");
+		      int start = 0;
+		      char buf[] = new char[end - start];
+		      starts.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	
+	}
+	      public void DownloadedSize () {
+	    	 
+	    	  String stats= driver.findElement(By.xpath("//*[@class='status']")).getText();
+	    	  if(stats.contains("–")) {
+	    	  	
+	    	  int start = stats.indexOf("–")+2;
+		      int end = stats.lastIndexOf("из");
+		      char buf[] = new char[end - start];
+		      stats.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	      else if (stats.contains("-")) {
+	    	  
+	    	  int start = stats.indexOf("–")+2;
+		      int end = stats.lastIndexOf("из");
+		      char buf[] = new char[end - start];
+		      stats.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	     }
+	      public void FileSize() {
+	    	  String stats= driver.findElement(By.xpath("//*[@class='status']")).getText();
+	    	  if(stats.contains("–")) {
+	    	  	
+	    	  int start = stats.indexOf("з")+2;
+		      int end = stats.lastIndexOf("МБ")+2;
+		      char buf[] = new char[end - start];
+		      stats.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	      else if (stats.contains("-")) {
+	    	  
+	    	  int start = stats.indexOf("з")+2;
+		      int end = stats.lastIndexOf("МБ")+2;
+		      char buf[] = new char[end - start];
+		      stats.getChars(start, end, buf, 0);
+		      System.out.println(buf);
+	      }
+	      }
+	    
 }
